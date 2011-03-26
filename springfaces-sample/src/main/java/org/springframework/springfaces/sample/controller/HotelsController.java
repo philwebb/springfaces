029c8845a73438b31f67661af06cef7dc127b7c1
philw <philw@96f92a50-1e92-45f8-a6b0-eb54d5336e60>
2011-03-26 11:22:04 +0000

    Hack
    git-svn-id: http://svn.xp-dev.com/svn/spring-faces-mvc/spring-faces/trunk@437 96f92a50-1e92-45f8-a6b0-eb54d5336e60

--- springfaces-sample/src/main/java/org/springframework/springfaces/sample/controller/HotelsController.java
+++ springfaces-sample/src/main/java/org/springframework/springfaces/sample/controller/HotelsController.java
@@ -3,6 +3,12 @@ package org.springframework.springfaces.sample.controller;
 import java.security.Principal;
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.springfaces.sample.controller.dunno.Dunno;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@NavigationRules({ @NavigationCase(on = "go", to = "mvc:@showHotel") })
 public class HotelsController {
 
+	@Autowired
 	private BookingService bookingService;
+
+	@Bean
+	private Dunno getDunno(@Value("#{criteria}") SearchCriteria searchCriteria) {
+		return new Dunno(searchCriteria, bookingService);
+	}
 
 	//Implicit view hotels/search
 	//On Search to hotels?#{searchCriteria}
