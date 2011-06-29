458113e88789abda46757f202e9854e37a462ac0
Phillip Webb <phil.webb@orbweaver.co.uk>
2011-06-29 22:03:32 +0100

    navigation annotations

---
+++ springfaces-mvc/src/main/java/org/springframework/springfaces/mvc/navigation/annotation/CompositeNavigationMappingFilter.java
@@ -0,0 +1,33 @@
+package org.springframework.springfaces.mvc.navigation.annotation;
+
+import org.springframework.springfaces.mvc.navigation.NavigationContext;
+import org.springframework.util.Assert;
+
+/**
+ * A composite {@link NavigationMappingFilter} that matches when all the specified filters match.
+ * 
+ * @author Phillip Webb
+ */
+public class CompositeNavigationMappingFilter implements NavigationMappingFilter {
+
+	private NavigationMappingFilter[] filters;
+
+	/**
+	 * Create a new {@link CompositeNavigationMappingFilter}.
+	 * @param filters the composite filters
+	 */
+	public CompositeNavigationMappingFilter(NavigationMappingFilter... filters) {
+		Assert.notNull(filters, "Filters must not be null");
+		Assert.noNullElements(filters, "Filters must not contain null elements");
+		this.filters = filters;
+	}
+
+	public boolean matches(NavigationContext context) {
+		for (NavigationMappingFilter filter : filters) {
+			if (!filter.matches(context)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ springfaces-mvc/src/test/java/org/springframework/springfaces/mvc/navigation/annotation/CompositeNavigationMappingFilterTest.java
@@ -0,0 +1,68 @@
+package org.springframework.springfaces.mvc.navigation.annotation;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.springfaces.mvc.navigation.NavigationContext;
+
+/**
+ * Tests for {@link CompositeNavigationMappingFilter}.
+ * 
+ * @author Phillip Webb
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CompositeNavigationMappingFilterTest {
+
+	@Rule
+	public ExpectedException thown = ExpectedException.none();
+
+	@Mock
+	private NavigationContext context;
+
+	@Test
+	public void shouldNeedArray() throws Exception {
+		thown.expect(IllegalArgumentException.class);
+		thown.expectMessage("Filters must not be null");
+		new CompositeNavigationMappingFilter((NavigationMappingFilter[]) null);
+	}
+
+	@Test
+	public void shouldNeedNoNullElements() throws Exception {
+		thown.expect(IllegalArgumentException.class);
+		thown.expectMessage("Filters must not contain null elements");
+		new CompositeNavigationMappingFilter((NavigationMappingFilter) null);
+	}
+
+	@Test
+	public void shouldMatchIfAllMatch() throws Exception {
+		NavigationMappingFilter f1 = mock(NavigationMappingFilter.class);
+		NavigationMappingFilter f2 = mock(NavigationMappingFilter.class);
+		NavigationMappingFilter f3 = mock(NavigationMappingFilter.class);
+		given(f1.matches(context)).willReturn(true);
+		given(f2.matches(context)).willReturn(true);
+		given(f3.matches(context)).willReturn(true);
+		CompositeNavigationMappingFilter composite = new CompositeNavigationMappingFilter(f1, f2, f3);
+		assertTrue(composite.matches(context));
+	}
+
+	@Test
+	public void shouldNotMatchIfAnyDoesNotMatch() throws Exception {
+		NavigationMappingFilter f1 = mock(NavigationMappingFilter.class);
+		NavigationMappingFilter f2 = mock(NavigationMappingFilter.class);
+		NavigationMappingFilter f3 = mock(NavigationMappingFilter.class);
+		given(f1.matches(context)).willReturn(true);
+		given(f2.matches(context)).willReturn(false);
+		given(f3.matches(context)).willReturn(true);
+		CompositeNavigationMappingFilter composite = new CompositeNavigationMappingFilter(f1, f2, f3);
+		assertFalse(composite.matches(context));
+	}
+
+}
