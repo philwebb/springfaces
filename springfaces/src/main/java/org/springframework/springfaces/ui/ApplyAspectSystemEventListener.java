c892067a93ffb8059d1cbcf7b2d462d9c53568d4
Phillip Webb <phil.webb@orbweaver.co.uk>
2011-07-30 19:17:59 +0100

    Apply aspects listener

--- springfaces/src/main/java/org/springframework/springfaces/ui/ApplyAspectSystemEventListener.java
+++ springfaces/src/main/java/org/springframework/springfaces/ui/ApplyAspectSystemEventListener.java
@@ -1,10 +1,17 @@
 package org.springframework.springfaces.ui;
 
+import javax.faces.component.UIComponent;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.PostAddToViewEvent;
 import javax.faces.event.SystemEvent;
 import javax.faces.event.SystemEventListener;
 
+/**
+ * A {@link SystemEventListener} that will ensure that an {@link UIComponent components} that are added to a
+ * {@link UIAspectGroup} are wrapped with {@link UIApplyAspects}.
+ * 
+ * @author Phillip Webb
+ */
 public class ApplyAspectSystemEventListener implements SystemEventListener {
 
 	public boolean isListenerForSource(Object source) {
@@ -18,5 +25,39 @@ public class ApplyAspectSystemEventListener implements SystemEventListener {
 	}
 
 	private void processPostAddToViewEvent(PostAddToViewEvent event) {
+		UIComponent component = event.getComponent();
+		wrapComponentAndChildrenWithUIApplyAspects(component);
+	}
+
+	private void wrapComponentAndChildrenWithUIApplyAspects(UIComponent component) {
+		wrapComponentWithUIApplyAspects(component);
+		if (component.getChildCount() > 0) {
+			for (UIComponent child : component.getChildren()) {
+				wrapComponentAndChildrenWithUIApplyAspects(child);
+			}
+		}
+	}
+
+	private void wrapComponentWithUIApplyAspects(UIComponent component) {
+		if (component.getParent() instanceof UIApplyAspects || component instanceof UIAspect) {
+			return;
+		}
+		UIAspectGroup aspectGroup = getParentAspectGroup(component);
+		if (aspectGroup != null) {
+			int index = component.getParent().getChildren().indexOf(component);
+			UIApplyAspects wrapper = new UIApplyAspects();
+			component.getParent().getChildren().set(index, wrapper);
+			wrapper.getChildren().add(component);
+		}
+	}
+
+	private UIAspectGroup getParentAspectGroup(UIComponent component) {
+		if (component == null) {
+			return null;
+		}
+		if (component instanceof UIAspectGroup) {
+			return (UIAspectGroup) component;
+		}
+		return getParentAspectGroup(component.getParent());
 	}
 }
--- springfaces/src/main/java/org/springframework/springfaces/ui/UIApplyAspects.java
+++ springfaces/src/main/java/org/springframework/springfaces/ui/UIApplyAspects.java
@@ -1,7 +1,15 @@
 package org.springframework.springfaces.ui;
 
+import javax.faces.component.UIComponent;
 import javax.faces.component.UIComponentBase;
 
+/**
+ * A wrapper for {@link UIComponent components} contained within an {@link UIAspectGroup} that is responsible for
+ * applying the relevant {@link UIAspect}s. This component is dynamically added via the
+ * {@link ApplyAspectSystemEventListener} and should not be used directly.
+ * 
+ * @author Phillip Webb
+ */
 public class UIApplyAspects extends UIComponentBase {
 
 	public static final String COMPONENT_FAMILY = "spring.faces.Aspect";
