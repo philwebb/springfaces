dd1f079d17a65ce46a5b3aa5f9197cf2f9267e8e
Phil Webb <pwebb@vmware.com>
2012-01-11 00:11:34 -0800

    fixup integration tests

--- springfaces-integrationtest/springfaces-showcase-mojarra-it/src/test/java/org/springframework/springfaces/traveladvisor/integrationtest/ConverterIT.java
+++ springfaces-integrationtest/springfaces-showcase-mojarra-it/src/test/java/org/springframework/springfaces/traveladvisor/integrationtest/ConverterIT.java
@@ -10,8 +10,8 @@ import org.springframework.springfaces.integrationtest.selenium.rule.Pages;
 import org.springframework.springfaces.traveladvisor.integrationtest.page.converter.FacesConverterByIdPage;
 import org.springframework.springfaces.traveladvisor.integrationtest.page.converter.FacesConverterForClassPage;
 import org.springframework.springfaces.traveladvisor.integrationtest.page.converter.GenericSpringBeanConverterPage;
-import org.springframework.springfaces.traveladvisor.integrationtest.page.converter.SpringBeanForClassConverterPage;
 import org.springframework.springfaces.traveladvisor.integrationtest.page.converter.SpringBeanConverterPage;
+import org.springframework.springfaces.traveladvisor.integrationtest.page.converter.SpringBeanForClassConverterPage;
 import org.springframework.springfaces.traveladvisor.integrationtest.rule.ShowcasePages;
 
 /**
@@ -26,25 +26,25 @@ public class ConverterIT {
 
 	@Test
 	public void shouldSupportFacesConverterForClass() throws Exception {
-		PageObject page = pages.get(FacesConverterForClassPage.class);
-		assertThat(page.getBodyText(), is("Conversion for class : 123 from genericSpringBeanConverter"));
+		PageObject page = this.pages.get(FacesConverterForClassPage.class);
+		assertThat(page.getBodyText(), is("Conversion for class : 123 from forClassFacesConverter"));
 	}
 
 	@Test
 	public void shouldSupportFacesConverterByID() throws Exception {
-		PageObject page = pages.get(FacesConverterByIdPage.class);
+		PageObject page = this.pages.get(FacesConverterByIdPage.class);
 		assertThat(page.getBodyText(), is("Conversion by ID : 123 from byIdFacesConverter"));
 	}
 
 	@Test
 	public void shouldSupportSpringBeanConverter() throws Exception {
-		PageObject page = pages.get(SpringBeanConverterPage.class);
+		PageObject page = this.pages.get(SpringBeanConverterPage.class);
 		assertThat(page.getBodyText(), is("Conversion by ID : 123 from springBeanConverter"));
 	}
 
 	@Test
 	public void shouldSupportGenericSpringBeanConverter() throws Exception {
-		GenericSpringBeanConverterPage page = pages.get(GenericSpringBeanConverterPage.class);
+		GenericSpringBeanConverterPage page = this.pages.get(GenericSpringBeanConverterPage.class);
 		page.setInputText("123");
 		page = page.clickSubmitButton();
 		assertThat(page.getConversionText(), is("Conversion by ID : 123 from genericSpringBeanConverter"));
@@ -52,7 +52,7 @@ public class ConverterIT {
 
 	@Test
 	public void shouldSupportSpringBeanForClass() throws Exception {
-		PageObject page = pages.get(SpringBeanForClassConverterPage.class);
+		PageObject page = this.pages.get(SpringBeanForClassConverterPage.class);
 		assertThat(page.getBodyText(), is("Conversion by Class : 123 from forClassSpringConverter"));
 	}
 }
