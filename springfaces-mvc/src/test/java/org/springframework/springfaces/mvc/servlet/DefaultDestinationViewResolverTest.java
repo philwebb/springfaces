f6458cdeabb95114623cf7aa70443e4578f86086
Phillip Webb <pwebb@vmware.com>
2012-06-05 17:33:08 -0700

    Added DefaultDestinationViewResolverTest

--- springfaces-mvc/src/test/java/org/springframework/springfaces/mvc/servlet/DefaultDestinationViewResolverTest.java
+++ springfaces-mvc/src/test/java/org/springframework/springfaces/mvc/servlet/DefaultDestinationViewResolverTest.java
@@ -15,7 +15,24 @@
  */
 package org.springframework.springfaces.mvc.servlet;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.Locale;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.springfaces.mvc.model.SpringFacesModel;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 /**
  * Tests for {@link DefaultDestinationViewResolver}.
@@ -24,9 +41,36 @@ import org.junit.Test;
  */
 public class DefaultDestinationViewResolverTest {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
 	@Test
-	public void test() {
-		// FIXME
+	public void shouldNeedDispatcher() throws Exception {
+		this.thrown.expect(IllegalArgumentException.class);
+		this.thrown.expectMessage("Dispatcher must not be null");
+		new DefaultDestinationViewResolver(null);
 	}
 
+	@Test
+	public void shouldDelegateToDispatcher() throws Exception {
+		Dispatcher dispatcher = mock(Dispatcher.class);
+		FacesContext context = mock(FacesContext.class);
+		ExternalContext externalContext = mock(ExternalContext.class);
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		Locale locale = Locale.US;
+		SpringFacesModel model = new SpringFacesModel();
+		String destination = "viewName";
+		View view = mock(View.class);
+
+		given(context.getExternalContext()).willReturn(externalContext);
+		given(externalContext.getRequest()).willReturn(request);
+		given(dispatcher.resolveViewName(destination, model, locale, request)).willReturn(view);
+
+		DefaultDestinationViewResolver resolver = new DefaultDestinationViewResolver(dispatcher);
+		ModelAndView resolved = resolver.resolveDestination(context, destination, locale, model);
+
+		verify(dispatcher).resolveViewName(destination, model, locale, request);
+		assertThat(resolved.getView(), is(view));
+		assertThat(resolved.getModel().size(), is(0));
+	}
 }
